ea0b904fe81cec63bb47261c6c024d4144006984
Indussolver <indusplusbusiness@gmail.com>
2026-03-24 19:37:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,6 @@
-// smooth scroll
-document.querySelectorAll("a[href^='#']").forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
-  });
-});
-
-// WhatsApp
 document.getElementById("whatsappBtn").addEventListener("click", function () {
   let number = "91XXXXXXXXXX";
-  let message = "Hi, I want a website for my business.";
+  let message = "Hi, I want a website";
   let url = `https://wa.me/${number}?text=${encodeURIComponent(message)}`;
   window.open(url, "_blank");
 });
